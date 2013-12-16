423aa75987e091d392063ea0fe9faaf45d7de803
Edouard KLEIN <edouard@Halvskugga.local>
2013-12-16 18:52:09 +0100

    RAMFeatures gives the 1024 bits of the RAM as ints

--- doc/examples/RAMFeatures.py
+++ doc/examples/RAMFeatures.py
@@ -1,14 +1,17 @@
 from FeatureAgent import *
 from rlglue.types import Observation, Action
+import numpy
+numpy_int_type = numpy.dtype('int32').newbyteorder('>')
 
 class RAMFeatures(FeatureAgent):
     def __init__(self):
         super().__init__()
+        self.binary_masks = [pow(2,i) for i in range(0,8)]
 
     def init_to_agent(self, task_spec):
         '''Our new task_spec string discards the screen info and only keeps the RAM, and ask only for the action from the first player'''
         #Old task_spec is : VERSION RL-Glue-3.0 PROBLEMTYPE episodic DISCOUNTFACTOR 1 OBSERVATIONS INTS (128 0 255)(33600 0 127) ACTIONS INTS (0 17)(18 35) REWARDS (UNSPEC UNSPEC) EXTRA Name: Arcade Learning Environment
-        return "VERSION RL-Glue-3.0 PROBLEMTYPE episodic DISCOUNTFACTOR 1 OBSERVATIONS INTS (128 0 255) ACTIONS INTS (0 17) REWARDS (UNSPEC UNSPEC) EXTRA Name: Arcade Learning Environment, Asterix game, RAM features"
+        return "VERSION RL-Glue-3.0 PROBLEMTYPE episodic DISCOUNTFACTOR 1 OBSERVATIONS INTS (1024 0 1) ACTIONS INTS (0 17) REWARDS (UNSPEC UNSPEC) EXTRA Name: Arcade Learning Environment, Asterix game, RAM features"
 
     def start_to_agent(self, observation):
         return self.new_observation(observation)
@@ -17,8 +20,11 @@ class RAMFeatures(FeatureAgent):
         return reward, self.new_observation(observation)
 
     def new_observation(self, observation):
-        answer = Observation(128,0,0)
-        answer.intArray = observation.intArray[:128]
+        #The 1024 bits of RAM of the Atari are passed as 1024 ints
+        answer = Observation(1024,0,0)
+        answer.intArray = numpy.zeros(1024,dtype=numpy_int_type)
+        for i in range(0,1024):
+            answer.intArray[i] = observation.intArray[i/8] & self.binary_masks[i%8]
         return answer
         
     def step_from_agent(self, action):
